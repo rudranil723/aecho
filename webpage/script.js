ca2547fe013e91f41a5f1d98c607f9d9995526f4
Ruddy <rudranilshil@gmail.com>
2023-04-07 21:23:12 +0530

    the background navbar and loading spinner done

--- webpage/script.js
+++ webpage/script.js
@@ -1,4 +1,22 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
+
+var loader = document.getElementById("pre-loader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+
 $('.carousel').carousel({
   pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#FFFF";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
