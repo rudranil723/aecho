9359a0a1ff8bb29be6b9b10f9201884447de0e0b
Ruddy <rudranilshil@gmail.com>
2023-05-15 18:59:59 +0530

    about us portion

--- webpage/final/script.js
+++ webpage/final/script.js
@@ -113,3 +113,38 @@ function stickyNavbar() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+// partners us about us js section
+
+
+
+const slider = document.querySelector('[data-carousel]');
+const slides = [...document.querySelectorAll('.Wallop-item')]
+this.wallop = new Wallop(slider);
+
+let prev = 0
+
+const removePrevClasses = (index) => {
+	let prevClass
+	if (slides[index].classList.contains('Wallop-item--hidePrevious')) {
+		prevClass = 'Wallop-item--hidePrevious'
+	} else if (slides[index].classList.contains('Wallop-item--hideNext')) {
+		prevClass = 'Wallop-item--hideNext'
+	}
+	
+	if (prevClass) {
+		setTimeout(() => {
+		slides[index].classList.remove(prevClass)
+	}, 600)
+	}
+}
+
+const onChange = () => {
+	removePrevClasses(prev)
+	prev = this.wallop.currentItemIndex
+}
+
+this.wallop.on('change', onChange);
+
+
