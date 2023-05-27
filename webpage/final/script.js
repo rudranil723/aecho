ad46940182b5539be9cbb5ef2f455f48e4705a17
Ruddy <rudranilshil@gmail.com>
2023-05-27 19:39:50 +0530

    almost there

--- webpage/final/script.js
+++ webpage/final/script.js
@@ -114,38 +114,4 @@ function stickyNavbar() {
   }
 }
 
-
-// partners us about us js section
-
-
-
-const slider = document.querySelector('[data-carousel]');
-const slides = [...document.querySelectorAll('.Wallop-item')]
-this.wallop = new Wallop(slider);
-
-let prev = 0
-
-const removePrevClasses = (index) => {
-	let prevClass
-	if (slides[index].classList.contains('Wallop-item--hidePrevious')) {
-		prevClass = 'Wallop-item--hidePrevious'
-	} else if (slides[index].classList.contains('Wallop-item--hideNext')) {
-		prevClass = 'Wallop-item--hideNext'
-	}
-	
-	if (prevClass) {
-		setTimeout(() => {
-		slides[index].classList.remove(prevClass)
-	}, 600)
-	}
-}
-
-const onChange = () => {
-	removePrevClasses(prev)
-	prev = this.wallop.currentItemIndex
-}
-
-this.wallop.on('change', onChange);
-
-
-// end of about us section the above js is for the partners section 
+// end of js for the home page
